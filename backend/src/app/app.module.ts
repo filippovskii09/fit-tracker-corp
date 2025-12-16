68aee08f04383d89410d564e7becff93c1064a26
artur.filippovskii <arturfilippovskij@gmail.com>
2025-12-16 19:36:49 +0200

    fix: add dockerignor & add trottler

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 import { envSchema } from '@src/config/env.validation';
 import { DBModule } from '@src/db/db.module';
@@ -12,8 +14,19 @@ import { DBModule } from '@src/db/db.module';
       envFilePath: '.env',
     }),
     DBModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
