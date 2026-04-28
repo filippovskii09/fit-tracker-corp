9991a65c87a6e45bb981155341d0244fa1e9ffa0
artur.filippovskii <arturfilippovskij@gmail.com>
2026-04-29 01:39:04 +0300

    feat: implement test cov for ci

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -63,10 +63,10 @@ const config: Config = {
 
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 
