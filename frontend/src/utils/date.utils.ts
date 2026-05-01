7d2bf973a20cb1d1d9a389a52014006dcda3f43c
artur.filippovskii <arturfilippovskij@gmail.com>
2026-05-02 01:45:48 +0300

    fix: remove extra uncovered code

--- frontend/src/utils/date.utils.ts
+++ frontend/src/utils/date.utils.ts
@@ -1,18 +1,3 @@
 export const toISODate = (year: number, monthIndex: number, day: number) => {
   return `${year}-${String(monthIndex + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
 };
-
-export const getDateStatusLabel = (date: Date, today = new Date()) => {
-  const dateTime = date.setHours(0, 0, 0, 0);
-  const todayTime = today.setHours(0, 0, 0, 0);
-
-  if (dateTime < todayTime) {
-    return 'past';
-  }
-
-  if (dateTime > todayTime) {
-    return 'future';
-  }
-
-  return 'today';
-};
