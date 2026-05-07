96f3343f25936f3a6ecbe2195b247a6598e20cbd
artur.filippovskii <arturfilippovskij@gmail.com>
2026-05-08 02:19:57 +0300

    ci: test fake data

--- frontend/src/utils/navigation.util.ts
+++ frontend/src/utils/navigation.util.ts
@@ -1,3 +1,19 @@
 export const redirectTo = (url: string): void => {
   window.location.href = url;
 };
+
+export const getLocation = () => {
+  return window.location.href;
+};
+
+export const getPreviousLocation = () => {
+  return window.history.back();
+};
+
+export const getNextLocation = () => {
+  return window.history.forward();
+};
+
+export const reloadPage = () => {
+  window.location.reload();
+};
