0788d7e28fcf7d41deee714ec79a04c48f2fb069
artur.filippovskii <arturfilippovskij@gmail.com>
2026-05-09 01:36:18 +0300

    ci: test ci fail

--- frontend/src/utils/navigation.utils.ts
+++ frontend/src/utils/navigation.utils.ts
@@ -1,3 +1,25 @@
 export const redirectTo = (url: string): void => {
   window.location.href = url;
 };
+
+export const currentLocation = () => {
+  return window.location.href;
+};
+
+export const nextLocation = (path: string): string => {
+  return `${currentLocation()}/${path}`;
+};
+
+export const previousLocation = (): string => {
+  const current = currentLocation();
+
+  if (current.endsWith('/')) {
+    return current.slice(0, current.length - 1);
+  }
+
+  return current.slice(0, current.lastIndexOf('/'));
+};
+
+export const back = (): void => {
+  window.history.back();
+};
