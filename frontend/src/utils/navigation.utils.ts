e3ba50dba1c2bee9ff1182cc762d76f737a7054a
artur.filippovskii <arturfilippovskij@gmail.com>
2026-05-09 03:11:15 +0300

    fix: remove extra code

--- frontend/src/utils/navigation.utils.ts
+++ frontend/src/utils/navigation.utils.ts
@@ -1,25 +1,3 @@
 export const redirectTo = (url: string): void => {
   window.location.href = url;
 };
-
-export const currentLocation = () => {
-  return window.location.href;
-};
-
-export const nextLocation = (path: string): string => {
-  return `${currentLocation()}/${path}`;
-};
-
-export const previousLocation = (): string => {
-  const current = currentLocation();
-
-  if (current.endsWith('/')) {
-    return current.slice(0, current.length - 1);
-  }
-
-  return current.slice(0, current.lastIndexOf('/'));
-};
-
-export const back = (): void => {
-  window.history.back();
-};
