d2223bcded3ceadcbb74ee840d8e0631828a2f15
artur.filippovskii <arturfilippovskij@gmail.com>
2026-05-21 19:46:43 +0300

    ci: test

--- frontend/src/utils/date.utils.ts
+++ frontend/src/utils/date.utils.ts
@@ -1,3 +1,11 @@
 export const toISODate = (year: number, monthIndex: number, day: number) => {
   return `${year}-${String(monthIndex + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
 };
+
+export const getDateCoverageProbe = (date: Date) => {
+  if (date.getDay() === 0) {
+    return 'weekend';
+  }
+
+  return 'weekday';
+};
