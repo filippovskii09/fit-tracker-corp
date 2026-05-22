5f0a70c0d25accf1e95f4bb13b8edd19dce08faa
artur.filippovskii <arturfilippovskij@gmail.com>
2026-05-22 14:53:50 +0300

    ci: test

--- frontend/src/utils/date.utils.ts
+++ frontend/src/utils/date.utils.ts
@@ -9,3 +9,21 @@ export const getDateCoverageProbe = (date: Date) => {
 
   return 'weekday';
 };
+
+export const getQuarterLabel = (date: Date) => {
+  const quarter = Math.floor(date.getMonth() / 3) + 1;
+
+  return `Q${quarter} ${date.getFullYear()}`;
+};
+
+export const isDateInPast = (date: Date, now = new Date()) => {
+  return date.getTime() < now.getTime();
+};
+
+export const clampDateRange = (start: Date, end: Date) => {
+  if (start.getTime() <= end.getTime()) {
+    return { start, end };
+  }
+
+  return { start: end, end: start };
+};
